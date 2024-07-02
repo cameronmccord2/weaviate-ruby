1449c1ba94a4cf1897c8bcfedda7a1401644271d
Cameron McCord <cameronmccord2@gmail.com>
2024-07-02 10:17:42 -0400

    Update vectorConfig name

--- lib/weaviate/schema.rb
+++ lib/weaviate/schema.rb
@@ -30,7 +30,7 @@ module Weaviate
       vector_index_type: nil,
       vector_index_config: nil,
       vectorizer: nil,
-      vectorizer_config: nil,
+      vector_config: nil,
       module_config: nil,
       inverted_index_config: nil,
       replication_config: nil
@@ -47,7 +47,7 @@ module Weaviate
         req.body["multiTenancyConfig"] = {enabled: true} unless multi_tenant.nil?
         req.body["invertedIndexConfig"] = inverted_index_config unless inverted_index_config.nil?
         req.body["replicationConfig"] = replication_config unless replication_config.nil?
-        req.body["vectorizerConfig"] = vectorizer_config unless vectorizer_config.nil?
+        req.body["vectorConfig"] = vector_config unless vector_config.nil?
       end
 
       response.body
